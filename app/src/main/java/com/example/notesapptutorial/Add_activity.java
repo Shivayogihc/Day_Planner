58a87e7d36327719b15caba7b9c8e0b38dce65bb
shivayogi <shivayogihc@gmail.com>
2021-08-12 23:18:24 +0530

    Day Planner app

---
+++ app/src/main/java/com/example/notesapptutorial/Add_activity.java
@@ -0,0 +1,14 @@
+package com.example.notesapptutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_activity);
+    }
+}
